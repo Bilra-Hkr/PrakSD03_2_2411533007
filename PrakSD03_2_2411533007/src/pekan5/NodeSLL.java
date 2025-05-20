c1232ef8f645ea2d8100faf5f73c7948e65e7159
Bilra-Hkr <derielchae@gmail.com>
2025-05-20 15:39:01 +0700

    Add files via upload

---
+++ PrakSD03_2_2411533007/src/pekan5/NodeSLL.java
@@ -0,0 +1,13 @@
+package pekan4;
+
+public class NodeSLL {
+		//Node bagian data
+		int data;
+		//Pointer ke node berikutnya
+		NodeSLL next;
+		//KOnstruktor menginisialisasi node dengan data
+		public NodeSLL (int data) {
+			this.data = data;
+			this.next = null;
+		}
+	}
